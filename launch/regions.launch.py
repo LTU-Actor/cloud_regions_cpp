f01762fddf0e8dfd6c051433a005054ce257e5eb
rkaddis <rkaddis@ltu.edu>
2025-05-28 11:26:30 -0400

    add an rviz file, option to start rviz from launch

--- launch/regions.launch.py
+++ launch/regions.launch.py
@@ -3,13 +3,22 @@ from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
+from launch.conditions import IfCondition
 
 import os
+
+viz_arg = DeclareLaunchArgument("rviz", default_value="false")
 
 config = os.path.join(
         get_package_share_directory('cloud_regions_cpp'),
         'config',
         'params.yaml'
+        )
+
+rviz_config = os.path.join(
+        get_package_share_directory('cloud_regions_cpp'),
+        'rviz',
+        'regions.rviz'
         )
 
 def regions():
@@ -23,8 +32,19 @@ def regions():
             ],
     )
     
+def rviz():
+    return Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config],
+        condition=IfCondition(LaunchConfiguration("rviz"))
+    )
+    
     
 def generate_launch_description():
     return LaunchDescription([
-        regions()
+        viz_arg,
+        regions(),
+        rviz(),
     ])
